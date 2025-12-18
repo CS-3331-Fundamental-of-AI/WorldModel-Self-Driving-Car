5ef1904a3635d1fdebad52c897a864a31f752af2
Ng Duy Duc <bachduc.june@gmail.com>
2025-12-18 15:45:10 +0700

    debug

--- JEPA/train_jepa.py
+++ JEPA/train_jepa.py
@@ -174,11 +174,14 @@ def train():
             # Move JEPA-2 batch to device if exists
             if j2_batch is not None:
                 # JEPA-2 returns a tuple: (local_graph, deltas, meta, deltas_aug)
-                # Need to collate manually or use tier2_collate_fn
+                # Need to collate use tier2_collate_fn
                 j2_batch = tier2_collate_fn(j2_batch)
                 # Move all tensors to device
                 for key in ["graph_feats", "graph_adj", "graph_mask", "clean_deltas", "aug_deltas", "traj_mask"]:
                     j2_batch[key] = j2_batch[key].to(DEVICE)
+                # **replace batch["j2"] with processed version**
+                batch["j2"] = j2_batch
+
 
             with autocast_ctx:
                 out = pipeline.step(batch)
