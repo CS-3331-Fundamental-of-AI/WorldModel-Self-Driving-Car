8e4e024e8dc79e7338fd1de30bf189e4394225ec
Ng Duy Duc <bachduc.june@gmail.com>
2025-12-29 14:57:35 +0700

    fix

--- JEPA/__init__.py
+++ JEPA/__init__.py
@@ -1,9 +1,9 @@
-from .JEPA_PrimitiveLayer.vjepa.model import PrimitiveLayerJepa
+from .JEPA_PrimitiveLayer.vjepa.model import PrimitiveLayerJEPA
 from .JEPA_SecondLayer import JEPA_Tier2_InverseAffordance, JEPA_Tier2_PhysicalAffordance
 from .JEPA_ThirdLayer import JEPA_Tier3_GlobalEncoding
 
 __all__ = [
-    "PrimitiveLayerJepa",
+    "PrimitiveLayerJEPA",
     "JEPA_Tier2_InverseAffordance",
     "JEPA_Tier2_PhysicalAffordance",
     "JEPA_Tier3_GlobalEncoding",
